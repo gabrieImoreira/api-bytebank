938c890cba1d829b5b1f8278f8d8f286f16f1318
gams <gabriel.antonioms@gmail.com>
2021-02-09 21:53:36 -0300

    first commit

---
+++ src/main/java/com/gams/apibytebank/ApiBytebankApplication.java
@@ -0,0 +1,21 @@
+package com.gams.apibytebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ApiBytebankApplication {
+
+    @RequestMapping("/")
+    String home(){
+        return "Hello World, here you will find a" +
+                " API simulating one bank interface, enjoy :)";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBytebankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gams/apibytebank/ApiBytebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gams.apibytebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBytebankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
